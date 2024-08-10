872a95a48f669e5981bfd5919c6db167a753cbf1
nk-knvlv <nk_knvlv@mail.ru>
2024-08-10 16:17:10 +0300

    add; config.py; новый ключ в конфиге

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ SECRET_KEY = 'dev',
 DATABASE = 'postgresql://postgres:root@localhost:5432/postgres'
 DATABASE_SUPER_KEY = 'postgresql://postgres:root@localhost:5432/postgres'
 SOMEKEY = 'somekey'
+CROCS = 'crocs'
